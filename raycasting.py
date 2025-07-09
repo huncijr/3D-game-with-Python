06ac695412a68eb03dcdf5a4ddb2b5b1db80a026
Hunor <hunor.burian08@gmail.com>
2025-07-09 18:34:49 +0300

    Update raycasting.py
    Calculating the distance from the walls

--- raycasting.py
+++ raycasting.py
@@ -43,15 +43,15 @@ class RayCasting:
             cos_a = math.cos(ray_angle)  
 
             # Vízszintes metszéspont keresése
-            y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)  # a következő vízszintes csempe iránya
+            y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)  
             depth_horizontal = (y_hor - oy) / sin_a  
             x_hor = ox + depth_horizontal * cos_a  
             delta_depth = dy / sin_a 
             dx = delta_depth * cos_a  
 
             for i in range(MAX_DEPTH): 
-                tile_horizontal = int(x_hor), int(y_hor)  # jelenlegi csempe koordináta
-                if tile_horizontal in self.game.map.world_map:  # ha falat talál
+                tile_horizontal = int(x_hor), int(y_hor)  
+                if tile_horizontal in self.game.map.world_map:  
                     texture_horizontal = self.game.map.world_map[tile_horizontal]
                     break
                 x_hor += dx  
@@ -59,15 +59,15 @@ class RayCasting:
                 depth_horizontal += delta_depth  
 
             
-            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)  # következő függőleges csempe iránya
-            depth_vertical = (x_vert - ox) / cos_a  # távolság a függőleges találatig
-            y_vert = oy + depth_vertical * sin_a  # y pozíció a falig
-            delta_depth = dx / cos_a  # lépésméret a következő függőleges vonalig
-            dy = delta_depth * sin_a  # y lépés
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)  
+            depth_vertical = (x_vert - ox) / cos_a  
+            y_vert = oy + depth_vertical * sin_a 
+            delta_depth = dx / cos_a 
+            dy = delta_depth * sin_a  
 
-            for i in range(MAX_DEPTH):  # ciklus a függőleges sugarakhoz
-                tile_vert = int(x_vert), int(y_vert)  # aktuális csempe koordináta
-                if tile_vert in self.game.map.world_map:  # ha falat talál
+            for i in range(MAX_DEPTH):
+                tile_vert = int(x_vert), int(y_vert)  
+                if tile_vert in self.game.map.world_map: 
                     texture_vertical =self.game.map.world_map[tile_vert]
                     break
                 x_vert += dx  # továbblépés
@@ -88,12 +88,12 @@ class RayCasting:
             
             depth *= math.cos(self.game.player.angle-ray_angle)
            
-            proj_height = SCREEN_DIST / (depth + 0.0001)  # 3D magasság kiszámítása
+            proj_height = SCREEN_DIST / (depth + 0.0001)  
 
             #ray casting result
             self.raycasting_result.append((depth,proj_height,texture,offset))
 
-            ray_angle += DELTA_ANGLE  # továbbhaladás a következő sugárra (következő képernyőoszlop)
+            ray_angle += DELTA_ANGLE 
 
     def update(self):
         self.ray_cast()  
